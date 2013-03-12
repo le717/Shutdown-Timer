6aa237a37e6d72d8c0d192447fabaad7cb4c9880
le717 <le717@e.mail>
2013-03-12 19:24:28 -0400

    Killed Python 2 and 3 code (won't work), began time checking (in the WRONG FUNCTION!)

--- ShutdownTimer.py
+++ ShutdownTimer.py
@@ -21,7 +21,8 @@
 # Copyright 2013 Triangle717 (http://triangle717.wordpress.com).
 
 
-import sys, os, time
+import sys, os
+from time import strftime, localtime, sleep
 
 app = "Shutdown Timer"
 version = "0.2"
@@ -29,12 +30,15 @@ creator = "Triangle717"
 
 def main():
     '''Main Menu'''
-    sys.stdout.write("\n{0} {1}, Copyright 2013 {2}\n".format(app, version, creator))
-    sys.stdout.write("\nPlease enter the time you want the computer to shutdown.\n")
-    sys.stdout.write("Hint: Use the 12 hour format, with the following layout: 'HH:MM'")
-    offtime = get_input("\n\n> ")
-    time.sleep(50)
-    raise SystemExit
+    print("\n{0} {1}, Copyright 2013 {2}\n".format(app, version, creator))
+    print("\nPlease enter the time you want the computer to shutdown.\n")
+    print("Hint: Use the 12 hour format, with the following layout: 'HH:MM'")
+    offtime = input("\n\n> ")
+    print("Do you want AM or PM?")
+    apm = input("\n\n> ")
+    one2tot24(offtime, apm)
+##    time.sleep(50)
+##    raise SystemExit
 ##sys.stdout.write("Shutdown started")
 ##time.sleep(offtime)
 ##sys.stdout.write("os.system('shutdown /r /t 0')")
@@ -42,11 +46,21 @@ def main():
 ##os.system("shutdown /r /t 0")
 
 
-def one2tot24(offtime):
+def one2tot24(offtime, apm):
     '''Converts 12-hour format to 24-hour format'''
 
     # The current time, as defined by the System Clock
     cur_time = strftime("%H:%M %p", localtime())
+    print(cur_time)
+
+##    if cur_time.lower() == offtime + apm:
+##        print("Shutdown now!")
+##        sleep(1)
+##        raise SystemExit
+##    else:
+##        print("Shutdown later")
+##        sleep(10)
+##        raise SystemExit
 
 
 # -- Resource links just for me. :) -- #
@@ -56,11 +70,4 @@ def one2tot24(offtime):
 #http://www.thewindowsclub.com/shutdown-restart-windows-8
 
 if __name__ == "__main__":
-    if sys.version_info >= (3,0):
-        # Use Python 3 input
-        get_input = input
-        main()
-    else:
-        # Use Python 2 input
-        get_input = raw_input
-        main()
+    main()
