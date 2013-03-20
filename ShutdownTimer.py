db79df243d3e62a4b62065261ce5b31c6ca28c15
le717 <le717@e.mail>
2013-03-20 15:59:23 -0400

    Update GPL block

--- ShutdownTimer.py
+++ ShutdownTimer.py
@@ -1,24 +1,24 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+"""
+    Shutdown Timer -  Small Windows Shutdown Timer
+    Copyright 2013 Triangle717 <http://triangle717.wordpress.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 
 # Shutdown Timer
 # https://github.com/le717/Shutdown-Timer
-# Copyright 2013 Triangle717 (http://triangle717.wordpress.com).
 
 # logging.BasicConfig() code based on example from A Byte of Python
 # http://www.swaroopch.com/notes/Python
